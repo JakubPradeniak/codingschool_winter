b6ae6f3e44dc13a681fda36a0588fdf0a3d360ab
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2023-11-27 21:01:49 +0100

    Feat: basic PHP examples

--- index.php
+++ index.php
@@ -43,3 +43,57 @@ $word = match ($number) {
 };
 
 echo "<br />", $word;
+
+$array = array("Škoda", "Mercedes", "Alfa Romeo", "Fiat");
+$array2 = ["Škoda", "Mercedes", "Alfa Romeo", "Fiat"];
+echo "<br />", $array[2],
+    "<br />", $array2[0],
+    "<br />", count($array2);
+
+$array[3] = "Volvo";
+
+echo "<br />", $array[3];
+
+$assoc_array = [
+    "brand" => "Škoda",
+    "model" => "Octavia III",
+    "year" => 2017
+];
+
+echo "<br />", $assoc_array["brand"], " - ", $assoc_array["model"], ", vyrobeno: ", $assoc_array["year"];
+
+// Vícerozměrné pole (2D) - například reprezentace dat z databáze
+$cars = [
+    [
+        "brand" => "Škoda",
+        "model" => "Octavia III",
+        "year" => 2017
+    ],
+    [
+        "brand" => "Mercedes",
+        "model" => "SLK",
+        "year" => 2006
+    ],
+    [
+        "brand" => "Peugeot",
+        "model" => "308",
+        "year" => 2008
+    ]
+];
+
+// Vypsání obsahu proměnné na obrazovku
+var_dump($cars);
+
+foreach ($cars as $index => $car) {
+    echo "<br />", $index, ": ", $car["brand"], " - ", $car["model"], ", vyrobeno: ", $car["year"];
+}
+
+echo "<br />";
+foreach ($assoc_array as $key => $value) {
+    echo "<br />", "Na klíči: ", $key, " je: ", $value;
+}
+echo "<br />";
+
+for ($i = 0; $i < count($cars); $i++) {
+    echo "<br />", $cars[$i]["brand"], " - ", $cars[$i]["model"], ", vyrobeno: ", $cars[$i]["year"];
+}
