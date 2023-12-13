0c61d438e23b139b1d8a412eabcb57d4c9bbf1f5
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2023-12-13 16:16:48 +0100

    Docs: add comments to router class

--- App/AppCore/Routing/Router.php
+++ App/AppCore/Routing/Router.php
@@ -8,6 +8,11 @@ use App\AppCore\Enums\HttpMethod;
 use Closure;
 use App\AppCore\Exceptions\RouteNotFoundException;
 
+/**
+ * Třída Router - zjednodušeně řečeno se jedná o rozcestník, který přerozděluje
+ * práci uvnitř PHP programu, podle toho, na kterou stránku příjde uživatel (rozlišuje
+ * se podle odkazu).
+ */
 class Router
 {
     /** @var Route[] $routes */
@@ -18,6 +23,7 @@ class Router
         $this->routes = [];
     }
 
+    // Soukromá metoda pro test, zda se cesta už zaregistrovaná
     private function routeExists(string $route, HttpMethod $method): bool
     {
         foreach ($this->routes as $currentRoute) {
@@ -32,6 +38,7 @@ class Router
         return false;
     }
 
+    // Soukromá metoda pro registraci nových cest.
     private function addRoute(
         HttpMethod $method,
         string $route,
@@ -40,6 +47,8 @@ class Router
     ): void
     {
         if (!$this->routeExists($route, $method)) {
+            // pokud cesta není již zaregistrována, tak ji přidáme do pole cest (zaregistrujeme)
+            // cesta je reprezentována Objektem Route, který obsahuje všechny potřebné informace.
             $this->routes[] = new Route($route, $method, $handler, $protected);
         }
     }
@@ -72,6 +81,7 @@ class Router
         return $this;
     }
 
+    // Metoda pro přerozdělování práce uvnitř aplikace.
     public function resolveRequest(): void
     {
         // URL kam přišel uživatel, ze kterého získáváme:
@@ -89,12 +99,15 @@ class Router
         // - jinak vezmeme metodu požadavku z $_SERVER
         $requestMethod = htmlspecialchars($_POST['_method'] ?? $_SERVER['REQUEST_METHOD']);
 
+        // Cyklem projdeme všechny zaregistrované cesty
         foreach ($this->routes as $route) {
-            // Cesty s dynamickými prvky budeme zapisovat následovně:
-            // /prispevek/{id} -> /prispevek/.*
-            // /prispevek/{id}/edit -> /prispevek/.*/edit (/prispevek/5/edit)
+            // Odkazy s dynamickými prvky budeme zapisovat následovně:
+            //  cesta v routeru -> regulární výraz pro hledání shody -> jak vypadá odkaz v prohlížeči (kam přišel uživatel)
+            //
+            // /prispevek/{id} -> /prispevek/.* -> /prispevek/5
+            // /prispevek/{id}/edit -> /prispevek/.*/edit -> /prispevek/5/edit
             // nebo
-            // /prispevek/{slug}/edit -> /prispevek/.*/edit
+            // /prispevek/{slug}/edit -> /prispevek/.*/edit -> /prispevek/jak-programovat-v-php/edit
             $routeRegex = preg_replace(
                 '|{.*}|',
                 '.*',
@@ -105,7 +118,7 @@ class Router
             // je shodný s cestou zaregistrovanou v Routeru
             $match = preg_match("|^$routeRegex$|", $requestRoute);
 
-            // Test jestli odkaz odpovídá aktuální cestě a zároveň jestli je správná i methoda požadavku
+            // Test jestli odkaz odpovídá aktuální cestě a zároveň jestli je správná i HTTP methoda požadavku
             if ($match === 1 && $route->method->value === $requestMethod) {
                 if ($route->protected && !isset($_SESSION['loggedUser'])) {
                     // Přesměrování na hlavní stránku
@@ -114,40 +127,57 @@ class Router
 
                 $params = []; // pomocná proměnná pro uložení parametrů z odkazu -> ['id' => 5]
 
+                // Pokud uživatel není na hlavní stránce aplikace -> vytovříme pole parametrů
                 if ($requestRoute !== '/') {
+                    // cestu zaregistrovanou v routeru i odkaz kam příšel uživatel
+                    // rozdelíme podle lomítka na jednotlivá "slova"
                     $routeParts = explode('/', $route->route);
                     $requestRouteParts = explode('/', $requestRoute);
 
+                    // cyklem projdeme rozdělenou cestu a hledáme zástupné identifikátory v {}
                     foreach ($routeParts as $index => $value) {
                         $idName = [];
+                        // Hledání shody se způsobem zápisu zástupných ID (např. {page})
                         preg_match(
                             '/(?<={).+?(?=})/',
                             $value,
                             $idName
                         );
 
+                        // pokud jsme našli shodu, přidáme ji do pole parametrů
                         if ($idName) {
+                            // v $idName[0] je slovo uvnitř {} -> ovšem nyní bez {}, závorky jsme
+                            // odstranili pomocí fce preg_match
+                            // takže vytváříme např ['id' => 5]
                             $params[$idName[0]] = $requestRouteParts[$index];
                         }
                     }
                 }
 
+                // test jestli je obsluha požadavku funkce -> pokud ano, zavoláme ji a zastavíme router pomocí return
                 if (is_callable($route->handler)) {
                     call_user_func($route->handler);
                     return;
                 }
 
+                // test jestli je obsluha třída -> první prvek v poli
                 if (class_exists($route->handler[0])) {
+                    // pokud ano, vytvoříme z ní nový objekt
                     $controller = new $route->handler[0](); // new JmenoKontroleru();
-
+                    // a otestujeme, zda obsahuje metodu, kterou jsme předali jako druhý prvek pole
                     if (method_exists($controller, $route->handler[1])) {
                         $method = $route->handler[1];
+                        // pokud ano, zavoláme ji a vyřídíme tak požadavek uživatele -> následně zastavíme router pomocí return
                         $controller->$method($params);
+                        return;
                     }
                 }
             }
         }
 
+        // Pokud jsme se dostali až sem, znamená to, že uživatel zadal do prohlížeče
+        // odkaz, pro který není zaregistrována žádná cesta -> vyhodíme výjimku, na kterou následně zareagujeme
+        // v jiné části programu
         throw new RouteNotFoundException("Route: $requestRoute not found!");
     }
 }
